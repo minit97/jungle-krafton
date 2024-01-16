248d9a97df788b8bedebd0fb0a6ae6a52ceab379
박현민 <dndb4902@naver.com>
2024-01-16 14:47:29 +0900

    # chapter05. 배포 작업

--- 04.ex01/app.py
+++ 04.ex01/app.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스�
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+client = MongoClient('mongodb://test:test@localhost',27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbjungle  # 'dbjungle'라는 이름의 db를 만들거나 사용합니다.
 
 
